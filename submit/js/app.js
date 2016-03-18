60a858ef0d67a1c75e132070527d39cb04ade1f2
andreaainara <andrea.ainara10@gmail.com>
2016-03-17 20:09:03 -0700

    jQuery problems

--- submit/js/app.js
+++ submit/js/app.js
@@ -1,7 +1,13 @@
 console.log("Sanity Check: JS is working!");
 
+
 $(document).ready(function() {
 
   $("#time").text(Date.now());
+
+  $("form").on("submit", function handleSubmit(event) {
+    event.preventDefault();
+    $("body").append(rick_roll_youtube_embed_code);
+  });
 
 });
