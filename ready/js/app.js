c3f60f8ac0f77aff0c34e9919b530aa8dc35d82c
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-03-17 18:26:09 -0700

    Edits none

--- ready/js/app.js
+++ ready/js/app.js
@@ -1,13 +1,33 @@
-/*
-  What needs to change so that the page says "Go!"?
-*/
 
-$(document).ready(function() {
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+		  <title>Click</title>
 
-  console.log("The page says:", $("h1").text());
-  if ($("h1").text() === "Go!") {
-    console.log("Success!");
-  } else {
-    console.log("Can you make it say \"Go!\"?");
-  }
-});
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/style.css">
+
+    <!-- JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+	</head>
+	<body>
+    <h1>Track your clicks!</h1>
+    <div id="imperatives">
+      <span>Fire the missiles!</span>
+      <span>Launch the Armada!</span>
+      <span>Charge!</span>
+      <span>Exterminate!</span>
+      <span>Into the breech!</span>
+      <span>This is Sparta!</span>
+      <span>Huzzah!</span>
+    </div>
+
+    <h2>You clicked...</h2>
+    <ul>
+      <!-- <li>"Fire the missiles!" at 123456789</li> -->
+    </ul>
+
+	</body>
+</html>
