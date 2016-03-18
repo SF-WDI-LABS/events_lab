25166c662c669cdeb603a1e5b12bee0e728dc09d
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-03-17 22:42:43 -0700

    Re-worked on jQuery-click, keypress, submit

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -1,16 +1,25 @@
-console.log("Sanity Check: JS is working!");
-$(document).ready(function(){
+$(document).ready(function() {
   var startTime;
   var endTime;
-  var total;
+  var running;
+  var totalTime;
+  $('body').keyup(function(event){
+    if(event.keyCode === 32){
+      if(!running){
+        endTime = Date.now();
+        console.log('end time is: ', endTime);
+        totalTime = ((endTime - startTime)/1000);
+        console.log('total time is: ', totalTime);
+        running = true;
+        $('#total-time').text(totalTime + " seconds");
+      }
+        else {
+          running = false;
+          startTime = Date.now();
+          console.log('start time is: ', startTime);
+     }
 
-  $(window).on("keypress", function handleKeypress(event) {
-    if (startTime) {
-      endTime = Date.now();
-      total = (endTime - startTime) / 1000;
-      $("#total-time").text(total + " seconds");
-    } else {
-      startTime = Date.now();
-    }
+      }
+   console.log(event.keyCode);
+    });
   });
-});
