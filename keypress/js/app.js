a35cec1c2d43c832bdbd40af1d00ae10b8807e1f
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-03-17 19:02:49 -0700

    Keypress solution added

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -1,3 +1,16 @@
 console.log("Sanity Check: JS is working!");
-$(document).ready(function() {
+$(document).ready(function(){
+  var startTime;
+  var endTime;
+  var total;
+
+  $(window).on("keypress", function handleKeypress(event) {
+    if (startTime) {
+      endTime = Date.now();
+      total = (endTime - startTime) / 1000;
+      $("#total-time").text(total + " seconds");
+    } else {
+      startTime = Date.now();
+    }
+  });
 });
