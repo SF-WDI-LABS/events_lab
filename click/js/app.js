ab73b81581a28bd9046f90dd2e89031d325d2ef6
ilterates <iltates@gmail.com>
2016-03-18 01:04:15 -0700

    Change is now working

--- click/js/app.js
+++ click/js/app.js
@@ -1,5 +1,11 @@
 console.log("Sanity Check: JS is working!");
-
+//var text = "string";
 $(document).ready(function() {
+$(this).find('span').on("click", function challenge1(event) {
+var $copy = $(this).text();
+$("ul").text($copy);
+console.log($copy);
+
+});
 
 });
