0cedcf188991ef900254440f0af45de7a0d18635
andreaainara <andrea.ainara10@gmail.com>
2016-03-18 09:02:05 -0700

    my solution vs. TA solution

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -19,3 +19,30 @@ $(document).ready(function(){
   });
 
 });
+
+
+//code from Adam as helped by TA
+$(document).ready(function() {
+ var startTime;
+ var endTime;
+ var running = false;
+ var totalTime;
+ $('body').keyup(function(event){
+   if(event.keyCode === 32){
+     if(running){
+       endTime = Date.now();
+       console.log('end time is: ', endTime);
+       totalTime = ((endTime - startTime)/1000);//
+       console.log('total time is: ', totalTime);
+       running = false;
+       $('#total-time').text(totalTime + " seconds");
+     } else {
+       running = true;
+       startTime = Date.now();
+       console.log('start time is: ', startTime);
+     }
+
+   }
+   console.log(event.keyCode);
+ });
+});
