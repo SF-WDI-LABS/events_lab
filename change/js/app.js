b7a93aaa19e04d60d5a37e1e6af5723d7cbe5353
andreaainara <andrea.ainara10@gmail.com>
2016-03-29 17:38:47 -0700

    change and things

--- change/js/app.js
+++ change/js/app.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
 
   $("#time").text(Date.now());
 
+  $("input.add").on("change", function handleCHnage(e) {
+    var left = $("input#left").val();
+    var right = $("input#right").val();
+    var leftVal = parseInt(left) || 0;
+    var rightVal = parseInt(right) || 0;
+    $("#total").val(total);
+  });
+
 });
